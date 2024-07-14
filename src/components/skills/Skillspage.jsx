f3f986daf777a03fcae2726ed18fc083bde5b6c6
Joe7oo7 <josephrexa@gmail.com>
2024-07-14 23:33:15 +0530

    add:revised section retry

--- src/components/skills/Skillspage.jsx
+++ src/components/skills/Skillspage.jsx
@@ -5,52 +5,52 @@ const Skillspage = () => {
     <div><div className="skills-container">
     <div className="skill">
        <h5>HTML5</h5>
-        <img className="img-fluid" src="/assets/icons/html.svg" alt="html-icon" /> 
+        <img className="img-fluid" src="/public/assets/icons/html.svg" alt="html-icon" /> 
     </div>
     <div className="skill">
        <h5>CSS3</h5>
        <img className="img-fluid" src="
-       /assets/icons/icons8-css.svg" alt="html-icon" />
+       /public/assets/icons/icons8-css.svg" alt="html-icon" />
  
     </div>
     <div className="skill">
        <h5>Javascript</h5>
-       <img className="img-fluid" src="/assets/icons/icons8-javascript.svg" alt="html-icon" />
+       <img className="img-fluid" src="/public/assets/icons/icons8-javascript.svg" alt="html-icon" />
  
     </div>
     <div className="skill">
        <h5>Bootstrap</h5>
-       <img className="img-fluid" src="/assets/icons/icons8-bootstrap.svg" alt="html-icon" />
+       <img className="img-fluid" src="/public/assets/icons/icons8-bootstrap.svg" alt="html-icon" />
  
     </div>
     <div className="skill">
        <h5>React</h5>
-       <img className="img-fluid" src="/assets/icons/icons8-react-js.svg" alt="html-icon" />
+       <img className="img-fluid" src="/public/assets/icons/icons8-react-js.svg" alt="html-icon" />
  
     </div>
     <div className="skill">
        <h5>Python</h5>
-       <img className="img-fluid" src="/assets/icons/python-svgrepo-com.svg" alt="html-icon" />
+       <img className="img-fluid" src="/public/assets/icons/python-svgrepo-com.svg" alt="html-icon" />
  
     </div>
     <div className="skill">
        <h5>Django</h5>
-       <img className="img-fluid" src="/assets/icons/django-svgrepo-com.svg" alt="html-icon" />
+       <img className="img-fluid" src="/public/assets/icons/django-svgrepo-com.svg" alt="html-icon" />
  
     </div>
     <div className="skill">
        <h5>MySQL</h5>
-       <img className="img-fluid" src="/assets/icons/mysql.svg" alt="html-icon" />
+       <img className="img-fluid" src="/public/assets/icons/mysql.svg" alt="html-icon" />
  
     </div>
     <div className="skill">
        <h5>Github</h5>
-       <img className="img-fluid" src="/assets/icons/github.svg" alt="html-icon" />
+       <img className="img-fluid" src="/public/assets/icons/github.svg" alt="html-icon" />
  
     </div>
     <div className="skill">
        <h5>AWS</h5>
-       <img className="img-fluid" src="/assets/icons/aws-svgrepo-com.svg" alt="html-icon"/>
+       <img className="img-fluid" src="/public/assets/icons/aws-svgrepo-com.svg" alt="html-icon"/>
     </div>
  </div></div>
   )
