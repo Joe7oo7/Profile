339f67079cda098edccda41d22b482c497d8b0f7
Joe7oo7 <josephrexa@gmail.com>
2024-07-15 00:23:15 +0530

    add:New Images resolved

--- src/components/skills/Skillspage.jsx
+++ src/components/skills/Skillspage.jsx
@@ -1,59 +1,93 @@
-import React from 'react'
-import './Skillspage.css'
+import React from "react";
+import "./Skillspage.css";
 const Skillspage = () => {
   return (
-    <div><div className="skills-container">
-    <div className="skill">
-       <h5>HTML5</h5>
-        <img className="img-fluid" src="public/assets/icons/html.svg" alt="html-icon" /> 
+    <div>
+      <div className="skills-container">
+        <div className="skill">
+          <h5>HTML5</h5>
+          <img
+            className="img-fluid"
+            src=".\Portfolio\public\assets\icons\html.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>CSS3</h5>
+          <img
+            className="img-fluid"
+            src="
+       ./Portfolio/public/assets/icons/icons8-css.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>Javascript</h5>
+          <img
+            className="img-fluid"
+            src="./Portfolio/public/assets/icons/icons8-javascript.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>Bootstrap</h5>
+          <img
+            className="img-fluid"
+            src="./Portfolio/public/assets/icons/icons8-bootstrap.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>React</h5>
+          <img
+            className="img-fluid"
+            src="./Portfolio/public/assets/icons/icons8-react-js.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>Python</h5>
+          <img
+            className="img-fluid"
+            src="./Portfolio/public/assets/icons/python-svgrepo-com.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>Django</h5>
+          <img
+            className="img-fluid"
+            src="./Portfolio/public/assets/icons/django-svgrepo-com.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>MySQL</h5>
+          <img
+            className="img-fluid"
+            src="./Portfolio/public/assets/icons/mysql.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>Github</h5>
+          <img
+            className="img-fluid"
+            src="./Portfolio/public/assets/icons/github.svg"
+            alt="html-icon"
+          />
+        </div>
+        <div className="skill">
+          <h5>AWS</h5>
+          <img
+            className="img-fluid"
+            src="./Portfolio/public/assets/icons/aws-svgrepo-com.svg"
+            alt="html-icon"
+          />
+        </div>
+      </div>
     </div>
-    <div className="skill">
-       <h5>CSS3</h5>
-       <img className="img-fluid" src="
-       public/assets/icons/icons8-css.svg" alt="html-icon" />
- 
-    </div>
-    <div className="skill">
-       <h5>Javascript</h5>
-       <img className="img-fluid" src="public/assets/icons/icons8-javascript.svg" alt="html-icon" />
- 
-    </div>
-    <div className="skill">
-       <h5>Bootstrap</h5>
-       <img className="img-fluid" src="public/assets/icons/icons8-bootstrap.svg" alt="html-icon" />
- 
-    </div>
-    <div className="skill">
-       <h5>React</h5>
-       <img className="img-fluid" src="public/assets/icons/icons8-react-js.svg" alt="html-icon" />
- 
-    </div>
-    <div className="skill">
-       <h5>Python</h5>
-       <img className="img-fluid" src="public/assets/icons/python-svgrepo-com.svg" alt="html-icon" />
- 
-    </div>
-    <div className="skill">
-       <h5>Django</h5>
-       <img className="img-fluid" src="public/assets/icons/django-svgrepo-com.svg" alt="html-icon" />
- 
-    </div>
-    <div className="skill">
-       <h5>MySQL</h5>
-       <img className="img-fluid" src="public/assets/icons/mysql.svg" alt="html-icon" />
- 
-    </div>
-    <div className="skill">
-       <h5>Github</h5>
-       <img className="img-fluid" src="public/assets/icons/github.svg" alt="html-icon" />
- 
-    </div>
-    <div className="skill">
-       <h5>AWS</h5>
-       <img className="img-fluid" src="public/assets/icons/aws-svgrepo-com.svg" alt="html-icon"/>
-    </div>
- </div></div>
-  )
-}
+  );
+};
 
-export default Skillspage
+export default Skillspage;
