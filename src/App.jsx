1433d896847f45e4f1d266fda882cbd63aee1544
Joe7oo7 <josephrexa@gmail.com>
2024-10-08 21:34:33 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Contactpage from "./components/contact/Contactpage";
 import Skillspage from "./components/skills/Skillspage";
 import Educationpage from "./components/education/Educationpage";
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import VoiceCommandHandler from "./components/voice/VoiceCommandHandler";
 import Projectspage from "./components/projects/Projectspage";
 
